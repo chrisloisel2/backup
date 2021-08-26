e7ccd3fd27c36ce15e4f979804e3a0be0569ca9c
Loisel CHRISTOPHER <lchristo@e1r9p20.42.fr>
2021-08-27 00:06:46 +0200

    fds

--- src/print.c
+++ src/print.c
@@ -6,56 +6,47 @@
 /*   By: lchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 20:29:13 by lchristo          #+#    #+#             */
-/*   Updated: 2019/10/11 15:01:27 by lchristo         ###   ########.fr       */
+/*   Updated: 2021/08/26 22:36:39 by lchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../swap.h"
 
-void    ft_print_lst(t_lst *s, t_stack *r)
+void	ft_print_lst(t_lst *s, t_stack *r)
 {
-    int i;
-    int cpy;
-    
-    if (r->lena > r->lenb)
-        i = r->lena - 1;
-    else
-        i = r->lenb - 1;
-    cpy = r->lenmax - 1;
-    printf("  état de la stack %d\n-----------------------\n", s->core);
-    printf("   {%d}            {%d}\nnum = ", r->lena, r->lenb);
-    while (cpy > 0)
-    {
-        printf("%d , ", r->tl[cpy]);
-        cpy--;
-    }
-    printf("%d\n", r->tl[cpy]);
-    while (i >= 0)
-    {
-        if (r->lena > i)
-            printf("|a[%d]",  r->a[i]);
-        else
-            printf("     ");
-        printf("    %d       ", i);
-        if (r->lenb > i)
-            printf("b[%d]|\n",  r->b[i]);
-        else
-            printf("\n");
-        i--;
-    }
-    printf("range = %d\n", s->range);
-    printf("\n");
+	int	i;
+
+	if (r->lena > r->lenb)
+		i = r->lena - 1;
+	else
+		i = r->lenb - 1;
+	printf("  état de la stack %d\n-----------------------\n", s->core);
+	printf("   {%d}            {%d}\nnum = ", r->lena, r->lenb);
+	while (i >= 0)
+	{
+		if (r->lena > i)
+			printf("|a[%d]", r->a[i]);
+		else
+			printf("     ");
+		printf("    %d       ", i);
+		if (r->lenb > i)
+			printf("b[%d]|\n", r->b[i]);
+		else
+			printf("\n");
+		i--;
+	}
+	printf("range = %d\n", s->range);
+	printf("\n");
 }
 
-void    ft_print_core(t_lst *s)
+void	ft_print_core(t_lst *s)
 {
-    printf("----------core = %d----------\n", s->core);
-    printf("  range[%d]\n", s->range);
-    printf(" posmax[%d]\n", s->posmax);
-    printf(" posmin[%d]\n", s->posmin);
-    printf("    max[%d]\n", s->max);
-    printf("    min[%d]\n", s->min);
-    printf("   push[%d]\n", s->top);
-    printf("   push[%d]\n", s->bot);
-
+	printf("----------core = %d----------\n", s->core);
+	printf("  range[%d]\n", s->range);
+	printf(" posmax[%d]\n", s->posmax);
+	printf(" posmin[%d]\n", s->posmin);
+	printf("    max[%d]\n", s->max);
+	printf("    min[%d]\n", s->min);
+	printf("   push[%d]\n", s->top);
+	printf("   push[%d]\n", s->bot);
 }
